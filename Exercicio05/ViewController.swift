fa62403d8387148813d2a608d68a88f9cc2966d7
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2023-05-10 20:46:21 -0300

    Initial Commit

---
+++ Exercicio05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercicio05
+//
+//  Created by Lucas Luis Ribeiro on 10/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
